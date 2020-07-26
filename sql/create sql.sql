6c6fc14482b2dd94ea4bb35031c3f4d3b6fd9f5c
Belbos <maplelyy@naver.com>
2020-07-27 00:07:39 +0900

    Table Sql 추가

---
+++ sql/create sql.sql
@@ -0,0 +1,12 @@
+
+-- User 
+CREATE TABLE `user` (
+	`num` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`email` VARCHAR(100) NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`pw` VARCHAR(100) NULL COLLATE 'utf8mb4_0900_ai_ci',
+	PRIMARY KEY (`num`) USING BTREE
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
